02e0d697bbc957f7c6e4c762f6a831199b9aab20
ksri0610 <ksri0610@gmail.com>
2025-02-24 20:24:44 +0530

    added files

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-03b8adbf322415fd0" # ap-south-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
